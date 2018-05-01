1b6bd1633fa3fbf86e222e039489a4af12a7a971
Mrucznik <mrucznix@gmail.com>
2018-05-01 19:51:40 +0200

    Travis CI config (#1)
    * Build scripts
    * Add travis.yml file

---
+++ CI_scripts/get_includes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Getting includes"
+mkdir ~/compiler/include
+cd ~/compiler/include
+
+echo "Getting SA-MP includes"
+mkdir samp_includes
+cd samp_includes
+wget -q http://files.sa-mp.com/samp037_svr_R2-1-1_win32.zip
+unzip -qq samp037_svr_R2-1-1_win32.zip
+cp -R pawno/include/* ../
+cd ..
+rm -R samp_includes
